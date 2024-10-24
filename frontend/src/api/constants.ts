2a19247e4bc408ff1c19c4ab3f4ae46c5e3b3f85
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-10-23 23:33:26 -0400

    Toast added when login

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -3,3 +3,11 @@ export const API_ROOT = "http://localhost:8000/api/v1";
 export const loginURL = `${API_ROOT}/users/create-session`;
 export const signupURL = `${API_ROOT}/users/signup`;
 export const createJobURL = `${API_ROOT}/users/createjob`;
+
+export const ToastStyle = {
+  style: {
+    borderRadius: "10px",
+    background: "#333",
+    color: "#fff",
+  },
+};
