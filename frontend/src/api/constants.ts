6872841318ba4f89755789becaecbd23bbcb7388
Vaishvi Shah <83919024+VaishviShah@users.noreply.github.com>
2024-11-01 17:20:24 -0400

    Revert "Vaishvi dev design notification icon"

--- frontend/src/api/constants.ts
+++ frontend/src/api/constants.ts
@@ -1,4 +1,4 @@
-export const API_ROOT = "http://localhost:8000/api/v1";
+const API_ROOT = "http://localhost:8000/api/v1";
 
 export const loginURL = `${API_ROOT}/users/create-session`;
 export const signupURL = `${API_ROOT}/users/signup`;
