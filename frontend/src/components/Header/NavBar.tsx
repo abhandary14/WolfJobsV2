670b2b3efbf28cd2f2becf99bda227d0e0bf1080
Vaishvi <vaishvi.sce20@sot.pdpu.ac.in>
2024-11-01 16:38:49 -0400

    Notification icon added and optimised code

--- frontend/src/components/Header/NavBar.tsx
+++ frontend/src/components/Header/NavBar.tsx
@@ -1,30 +1,11 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useUserStore } from "../../store/UserStore";
 import NavBarItem from "./NavBarItem";
 
-const NavBar = () => {
+const NavBar = ({ notificationCount }: { notificationCount: any }) => {
   const isLoggedIn = useUserStore((state) => state.isLoggedIn);
   const role = useUserStore((state) => state.role);
-
-  const [notificationCount, setNotificationCount] = useState(0);
-
-  useEffect(() => {
-    if (isLoggedIn && role === "Applicant") {
-      axios.get('http://localhost:8000/api/v1/users/fetchapplications')
-        .then((res) => {
-          if (res.status === 200) {
-            const applications = res.data.application;
-            const acceptedCount = applications.filter(app => app.status === 'accepted').length;
-            const rejectedCount = applications.filter(app => app.status === 'rejected').length;
-            setNotificationCount(acceptedCount + rejectedCount);
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching applications:', error);
-        });
-    }
-  }, [isLoggedIn, role]);
 
   return (
     <>
@@ -32,8 +13,15 @@ const NavBar = () => {
         <nav className="text-sm font-semibold leading-6 text-slate-700 ">
           <ul className="flex space-x-8">
             {isLoggedIn && <NavBarItem link="/profile" text="Profile" />}
-            {isLoggedIn && role == "Applicant" && <NavBarItem link="/resume" text="Upload Resume" />}
-            {isLoggedIn && role === "Applicant" && <NavBarItem link="/notifications" text={`Notifications (${notificationCount})`} />}
+            {isLoggedIn && role == "Applicant" && (
+              <NavBarItem link="/resume" text="Upload Resume" />
+            )}
+            {isLoggedIn && role === "Applicant" && (
+              <NavBarItem
+                link="/notifications"
+                text={`Notifications (${notificationCount})`}
+              />
+            )}
             {isLoggedIn && <NavBarItem link="/logout" text="Log Out" />}
           </ul>
         </nav>
