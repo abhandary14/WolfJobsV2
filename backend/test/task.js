1da0d0093bafabc79676aa7fbb79a2415fc45915
AakashBhandary <aakashbhandary2024@gmail.com>
2024-11-02 00:28:12 -0400

    created test case for sending job acceptance

--- backend/test/task.js
+++ backend/test/task.js
@@ -111,12 +111,12 @@ describe("Tasks API", function () {
       });
     });
 
-    describe("POST /send/send-job-rejection-email", () => {
-      it("It should send a job rejection email", (done) => {
+    describe("POST /send/send-job-acceptance-email", () => {
+      it("It should send a job acceptance email", (done) => {
         const emailBody = {
           applicationId: "application123",
           jobid: "job456",
-          emailType: "rejection",
+          emailType: "acceptance",
           applicantEmail: "applicant@example.com",
           applicantName: "John Doe",
           jobTitle: "Software Engineer",
@@ -126,42 +126,71 @@ describe("Tasks API", function () {
 
         chai
           .request("http://localhost:8000")
-          .post("/send/send-job-rejection-email")
+          .post("/send/send-job-acceptance-email")
           .send(emailBody)
           .end((err, response) => {
             if (err) return done(err);
-            response.should.have.status(200);
+            response.should.have.status(201);
             response.body.should.be.a("object");
-
+            response.body.should.have
+              .property("message")
+              .eql("Application accepted and email sent.");
             console.log("Response:", response.body);
             done();
           });
       });
-    });
 
-    describe("POST /send/forgot-password", () => {
-      it("It should initiate the forgot password process", (done) => {
-        const body = {
-          email: "priyanshumalaviya9@gmail.com",
-        };
+      describe("POST /send/send-job-rejection-email", () => {
+        it("It should send a job rejection email", (done) => {
+          const emailBody = {
+            applicationId: "application123",
+            jobid: "job456",
+            emailType: "rejection",
+            applicantEmail: "applicant@example.com",
+            applicantName: "John Doe",
+            jobTitle: "Software Engineer",
+            companyName: "NCSU",
+            contactEmail: "contact@ncsu.edu",
+          };
 
-        chai
-          .request("http://localhost:8000")
-          .post("/send/forgot-password")
-          .send(body)
-          .end(async (err, response) => {
-            if (err) return done(err);
-            response.should.have.status(200);
-            // response.body.should.be.a("object");
-            // response.body.should.have
-            //   .property("message")
-            //   .eql(`Reset Token has been sent to ${testUser.email}`);
-            console.log("Response:", response.body);
+          chai
+            .request("http://localhost:8000")
+            .post("/send/send-job-rejection-email")
+            .send(emailBody)
+            .end((err, response) => {
+              if (err) return done(err);
+              response.should.have.status(200);
+              response.body.should.be.a("object");
 
-            done();
-          });
+              console.log("Response:", response.body);
+              done();
+            });
+        });
+      });
+
+      describe("POST /send/forgot-password", () => {
+        it("It should initiate the forgot password process", (done) => {
+          const body = {
+            email: "priyanshumalaviya9@gmail.com",
+          };
+
+          chai
+            .request("http://localhost:8000")
+            .post("/send/forgot-password")
+            .send(body)
+            .end(async (err, response) => {
+              if (err) return done(err);
+              response.should.have.status(200);
+              // response.body.should.be.a("object");
+              // response.body.should.have
+              //   .property("message")
+              //   .eql(`Reset Token has been sent to ${testUser.email}`);
+              console.log("Response:", response.body);
+
+              done();
+            });
+        });
       });
     });
-
   });
 });
