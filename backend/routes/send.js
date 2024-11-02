175a2eef949600c633a66240b8470b74e477fc0e
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-11-01 21:24:24 -0400

    forgotPasswordURL, resetPasswordURL is added in a constant file

--- backend/routes/send.js
+++ backend/routes/send.js
@@ -1,14 +1,12 @@
 const express = require("express");
-
-const router = express.Router();
-
 const {
   sendJobAcceptanceEmail,
   sendJobRejectionEmail,
   sendJobSelectionEmail,
-  forgotPassword,
   resetPassword,
 } = require("../controllers/email_controller");
+
+const router = express.Router();
 
 router.post("/send-job-acceptance-email", sendJobAcceptanceEmail);
 router.post("/send-job-rejection-email", sendJobRejectionEmail);
