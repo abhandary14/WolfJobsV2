796462bf4d44475de5851356b2ff5c980ec3412f
AakashBhandary <aakashbhandary2024@gmail.com>
2024-11-02 00:19:42 -0400

    Added test case for creating job

--- backend/test/task.js
+++ backend/test/task.js
@@ -19,4 +19,56 @@ describe("Tasks API", function () {
       mongoose.connection.on("error", done);
     }
   });
+
+  describe("GET /api/v1/users/fetchapplications", () => {
+    it("IT SHOULD RETURN ALL THE APPLICATIONS", (done) => {
+      // const task = {
+      //     email:'shaangzb@gmail.com',
+      //     password:'123',
+
+      // };
+
+
+
+
+      describe("GET /api/v1/users/", () => {
+        it("IT SHOULD RETURN ALL THE JOBS", (done) => {
+          // const task = {
+          //     email:'shaangzb@gmail.com',
+          //     password:'123',
+
+          // };
+
+          chai
+            .request("http://localhost:8000")
+            .get("/api/v1/users/")
+
+            .end((err, response) => {
+              response.body.should.be.a("object");
+
+              console.log("*********", response.body);
+
+              done();
+            });
+        });
+      });
+
+      describe("POST /api/v1/users/createjob", () => {
+        it("IT SHOULD RETURN THE JOB", (done) => {
+          const body = {
+            name: "Shaan",
+            managerid: "1234556",
+            skills: "C,java",
+            location: "Noida",
+            description: "xyz",
+            pay: "10",
+            schedule: "10/10/10",
+          };
+          done();
+        });
+      });
+    });
+
+
+  });
 });
