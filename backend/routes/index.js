dfefec957df8a060522f61978dace7cf65b9f76b
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-11-01 22:47:15 -0400

    bug fixed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -11,7 +11,7 @@ router.use("/users", require("./users"));
 
 router.use("/api", require("./api"));
 router.use("/send", require("./send"));
-// router.use("/auth", require("./auth"));
+router.use("/auth", require("./auth"));
 router.use("/resume", require("./resume"));
 
 module.exports = router;
