17dfe36949aeb59c06ddb86ff573a069ac115781
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-11-01 20:39:33 -0400

    Created a controller logic to send Job Acceptance Email to applicants.

--- backend/controllers/email_controller.js
+++ backend/controllers/email_controller.js
@@ -1,4 +1,3 @@
-const { Resend } = require("resend");
 const nodemailer = require("nodemailer");
 const {
   jobApplicationAcceptedTemplate,
@@ -20,3 +19,57 @@ const transporter = nodemailer.createTransport({
     pass: process.env.SMTP_PASSWORD,
   },
 });
+
+module.exports.sendJobAcceptanceEmail = async (req, res) => {
+  const {
+    applicationId,
+    jobid,
+    emailType,
+    applicantEmail,
+    applicantName,
+    jobTitle,
+    companyName,
+    contactEmail,
+  } = req.body;
+
+  console.log(
+    applicationId,
+    jobid,
+    emailType,
+    applicantEmail,
+    applicantName,
+    jobTitle,
+    companyName,
+    contactEmail
+  );
+
+  const emailHtml = jobApplicationAcceptedTemplate({
+    applicantName: applicantName,
+    jobTitle: jobTitle,
+    companyName: companyName,
+    contactEmail: contactEmail,
+    applicationDate: new Date().toLocaleDateString(),
+    nextSteps:
+      "Please log in to your account to complete a brief assessment quiz. This will help us move forward in the hiring process.",
+  });
+
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL, // sender address
+      to: applicantEmail, // list of receivers
+      subject: `Your Application for ${jobTitle} has been Accepted`, // Subject line
+      html: emailHtml, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    return res.status(201).json({
+      success: true,
+      message: "Application accepted and email sent.",
+      data: info,
+      messageId: info.messageId,
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
