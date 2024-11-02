27ea9639d61fd4d7550dd942ceda95306e52bc72
Priyanshu9898 <priyanshu.mce20@sot.pdpu.ac.in>
2024-11-01 21:06:54 -0400

    reset password route is created

--- backend/routes/send.js
+++ backend/routes/send.js
@@ -14,6 +14,6 @@ router.post("/send-job-acceptance-email", sendJobAcceptanceEmail);
 router.post("/send-job-rejection-email", sendJobRejectionEmail);
 router.post("/selection-email", sendJobSelectionEmail);
 router.post("/forgot-password", forgotPassword);
-// router.post("/reset-password", resetPassword);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
