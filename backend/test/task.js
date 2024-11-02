7e5f38f5e8b9c88b57018aecc1f84e31d7382743
AakashBhandary <aakashbhandary2024@gmail.com>
2024-11-02 02:23:17 -0400

    created test case for creating a job

--- backend/test/task.js
+++ backend/test/task.js
@@ -111,6 +111,40 @@ describe("Tasks API", function () {
       });
     });
 
+    describe("POST /api/v1/users/createjob", () => {
+      it("IT SHOULD RETURN THE JOB", (done) => {
+        const body = {
+          name: "Shaan",
+          managerid: "1234556",
+          skills: "C,java",
+          location: "Noida",
+          description: "xyz",
+          pay: "10",
+          schedule: "10/10/10",
+        };
+  
+        chai
+          .request("http://localhost:8000")
+          .post("/api/v1/users/createjob")
+          .send({
+            name: "Shaan",
+            managerid: "1234556",
+            skills: "C,java",
+            location: "Noida",
+            description: "xyz",
+            pay: "10",
+            schedule: "10/10/10",
+          })
+          .end((err, response) => {
+            response.body.should.be.a("object");
+  
+            console.log("*********", response.body);
+  
+            done();
+          });
+      });
+    });
+
     describe("GET /api/v1/users/search", () => {
       it("IT SHOULD RETURN THE SEARCHED JOB", (done) => {
         const body = {
